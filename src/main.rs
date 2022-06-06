5ee9627f2b2f69e662711f48f93080e445e99bc2
Tarek <tarekspeed7@gmail.com>
2022-06-06 15:32:20 +0200

    Initial commit. Opens a window and clears it with a specific color

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+extern crate gl;
+extern crate glfw;
+
+use gl::types::*;
+use glfw::{Action, Context, Key};
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+
+    let (mut window, events) = glfw
+        .create_window(
+            300,
+            300,
+            "Hello, this is a window.",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    unsafe {
+        gl::Viewport(0, 0, 300, 300);
+        gl::ClearColor(0.2f32, 0.3f32, 0.3f32, 1.0f32);
+    }
+
+    while !window.should_close() {
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        window.swap_buffers();
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true);
+        }
+        _ => {}
+    }
+}
