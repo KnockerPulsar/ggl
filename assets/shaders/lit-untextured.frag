3eb174c80b521bbe28efcbca7b02f1441a6ea802
Tarek <tarekspeed7@gmail.com>
2022-06-08 00:48:35 +0200

    Add light mapps (diffuse, specular, emissive)

--- assets/shaders/lit-untextured.frag
+++ assets/shaders/lit-untextured.frag
@@ -1,9 +1,9 @@
 #version 330 core
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
+    sampler2D emissive;
     float shininess;
 }; 
   
@@ -17,6 +17,7 @@ struct Light {
 
 in vec3 normal;
 in vec3 frag_pos;
+in vec2 tex_coord;
 
 out vec4 frag_color;
 
@@ -26,21 +27,22 @@ uniform Light u_light;
 
 void main() {
     
-    vec3 ambient = u_material.ambient * u_light.ambient;
     
     vec3 norm = normalize(normal);
     vec3 light_dir = normalize(u_light.position- frag_pos);    
 
-    float diff = max(dot(norm, light_dir), 0);
-    vec3 diffuse = (diff * u_material.diffuse) * u_light.diffuse;
-    
     vec3 view_dir = normalize(u_view_pos - frag_pos);
     vec3 reflect_dir = reflect(-light_dir, norm);    
 
+    float diff = max(dot(norm, light_dir), 0);
     float spec = pow(max(dot(view_dir, reflect_dir), 0.0), u_material.shininess);
-    vec3 specular =(u_material.specular * spec) * u_light.specular;
+
+    vec3 ambient =  u_light.ambient * texture(u_material.diffuse, tex_coord).rgb;
+    vec3 diffuse =  diff * u_light.diffuse * texture(u_material.diffuse, tex_coord).rgb;
+    vec3 specular = spec * u_light.specular * texture(u_material.specular, tex_coord).rgb ;
+    vec3 emissive = texture(u_material.emissive, tex_coord).rgb;
     
-    vec3 result = (ambient + diffuse + specular);
+    vec3 result = (ambient + diffuse + specular + emissive);
     
     frag_color = vec4(result, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -49,20 +49,25 @@ fn main() {
 
     // 3 floats for vertex position
     // 3 floats for vertex normals
+    // 2 floats for texture coordinates
     let verts = vec![
-        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.5, 0.5, -0.5, 0.0,
-        0.0, -1.0, 0.5, 0.5, -0.5, 0.0, 0.0, -1.0, -0.5, 0.5, -0.5, 0.0, 0.0, -1.0, -0.5, -0.5,
-        -0.5, 0.0, 0.0, -1.0, -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.5,
-        0.5, 0.5, 0.0, 0.0, 1.0, 0.5, 0.5, 0.5, 0.0, 0.0, 1.0, -0.5, 0.5, 0.5, 0.0, 0.0, 1.0, -0.5,
-        -0.5, 0.5, 0.0, 0.0, 1.0, -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, -0.5, 0.5, -0.5, -1.0, 0.0, 0.0,
-        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, -0.5, -0.5, 0.5, -1.0,
-        0.0, 0.0, -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 0.5, 0.5, -0.5,
-        1.0, 0.0, 0.0, 0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.5, -0.5,
-        0.5, 1.0, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.0, -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.5,
-        -0.5, -0.5, 0.0, -1.0, 0.0, 0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 0.5, -0.5, 0.5, 0.0, -1.0, 0.0,
-        -0.5, -0.5, 0.5, 0.0, -1.0, 0.0, -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, -0.5, 0.5, -0.5, 0.0,
-        1.0, 0.0, 0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 0.5, 0.5, 0.5, 0.0,
-        1.0, 0.0, -0.5, 0.5, 0.5, 0.0, 1.0, 0.0, -0.5, 0.5, -0.5, 0.0, 1.0, 0.0f32,
+        -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0, 0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 0.0, 0.5,
+        0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0, 0.5, 0.5, -0.5, 0.0, 0.0, -1.0, 1.0, 1.0, -0.5, 0.5,
+        -0.5, 0.0, 0.0, -1.0, 0.0, 1.0, -0.5, -0.5, -0.5, 0.0, 0.0, -1.0, 0.0, 0.0, -0.5, -0.5,
+        0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 0.0, 0.5, 0.5, 0.5, 0.0,
+        0.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 0.0, 0.0, 1.0, 1.0, 1.0, -0.5, 0.5, 0.5, 0.0, 0.0, 1.0,
+        0.0, 1.0, -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 0.0, 0.0, -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0,
+        0.0, -0.5, 0.5, -0.5, -1.0, 0.0, 0.0, 1.0, 1.0, -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0,
+        -0.5, -0.5, -0.5, -1.0, 0.0, 0.0, 0.0, 1.0, -0.5, -0.5, 0.5, -1.0, 0.0, 0.0, 0.0, 0.0,
+        -0.5, 0.5, 0.5, -1.0, 0.0, 0.0, 1.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0, 0.0, 1.0, 0.0, 0.5, 0.5,
+        -0.5, 1.0, 0.0, 0.0, 1.0, 1.0, 0.5, -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 1.0, 0.5, -0.5, -0.5,
+        1.0, 0.0, 0.0, 0.0, 1.0, 0.5, -0.5, 0.5, 1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.0,
+        0.0, 1.0, 0.0, -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0, 0.5, -0.5, -0.5, 0.0, -1.0, 0.0,
+        1.0, 1.0, 0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0, 0.0, 0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 1.0,
+        0.0, -0.5, -0.5, 0.5, 0.0, -1.0, 0.0, 0.0, 0.0, -0.5, -0.5, -0.5, 0.0, -1.0, 0.0, 0.0, 1.0,
+        -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 1.0, 1.0, 0.5,
+        0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0, 0.5, 0.5, 0.5, 0.0, 1.0, 0.0, 1.0, 0.0, -0.5, 0.5, 0.5,
+        0.0, 1.0, 0.0, 0.0, 0.0, -0.5, 0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0f32,
     ];
 
     println!(
@@ -73,6 +78,10 @@ fn main() {
             .into_string()
             .unwrap()
     );
+
+    let container_diff_id = load_texture("assets/textures/container2.png");
+    let container_spec_id = load_texture("assets/textures/container2_specular.png");
+    let container_emissive_id = load_texture("assets/textures/container2_emissive.png");
 
     let lit_shader = shader::ShaderProgram::new(
         "assets/shaders/lit-untextured.vert",
@@ -108,7 +117,7 @@ fn main() {
             3,
             gl::FLOAT,
             gl::FALSE,
-            6 * size_of::<f32>() as i32,
+            8 * size_of::<f32>() as i32,
             std::ptr::null(),
         );
 
@@ -119,11 +128,22 @@ fn main() {
             3,
             gl::FLOAT,
             gl::FALSE,
-            6 * size_of::<f32>() as i32,
+            8 * size_of::<f32>() as i32,
             (3 * size_of::<f32>()) as *const _,
         );
 
         gl::EnableVertexAttribArray(1);
+
+        gl::VertexAttribPointer(
+            2,
+            2,
+            gl::FLOAT,
+            gl::FALSE,
+            8 * size_of::<f32>() as i32,
+            (6 * size_of::<f32>()) as *const _,
+        );
+
+        gl::EnableVertexAttribArray(2);
 
         light_shader.use_program();
 
@@ -138,13 +158,14 @@ fn main() {
             3,
             gl::FLOAT,
             gl::FALSE,
-            6 * size_of::<f32>() as i32, // Skip texture coordinates and normals
+            8 * size_of::<f32>() as i32, // Skip texture coordinates and normals
             std::ptr::null(),
         );
 
         gl::EnableVertexAttribArray(0);
 
         gl::Enable(gl::DEPTH_TEST);
+        gl::Enable(gl::TEXTURE_2D);
     }
 
     let mut last_frame = 0.0f32;
@@ -191,10 +212,20 @@ fn main() {
             lit_shader.use_program();
             lit_shader.set_vec3("u_view_pos", camera.get_pos());
 
-            lit_shader.set_vec3("u_material.ambient", glm::vec3(1.0, 0.5, 0.31));
-            lit_shader.set_vec3("u_material.diffuse", glm::vec3(1.0, 0.5, 0.31));
             lit_shader.set_vec3("u_material.specular", glm::vec3(0.5, 0.5, 0.5));
             lit_shader.set_float("u_material.shininess", 32.0);
+
+            lit_shader.set_int("u_material.diffuse", 0);
+            gl::ActiveTexture(gl::TEXTURE0);
+            gl::BindTexture(gl::TEXTURE_2D, container_diff_id);
+
+            lit_shader.set_int("u_material.specular", 1);
+            gl::ActiveTexture(gl::TEXTURE1);
+            gl::BindTexture(gl::TEXTURE_2D, container_spec_id);
+
+            lit_shader.set_int("u_material.emissive", 2);
+            gl::ActiveTexture(gl::TEXTURE2);
+            gl::BindTexture(gl::TEXTURE_2D, container_emissive_id);
 
             lit_shader.set_vec3("u_light.ambient", glm::vec3(0.2, 0.2, 0.2));
             lit_shader.set_vec3("u_light.diffuse", glm::vec3(0.5, 0.5, 0.5)); // darken diffuse light a bit
@@ -219,4 +250,58 @@ fn main() {
         last_frame = current_frame;
         window.swap_buffers();
     }
+}
+
+fn load_texture(path: &str) -> u32 {
+    let texture = image::io::Reader::open(path).unwrap().decode().unwrap();
+
+    let texture_w = texture.width() as i32;
+    let texture_h = texture.height() as i32;
+
+    let mut texture_id = 0u32;
+
+    unsafe {
+        let format = match texture.color() {
+            image::ColorType::L8 => gl::RED,
+            image::ColorType::Rgb8 => gl::RGB,
+            image::ColorType::Rgba8 => gl::RGBA,
+            _ => {
+                panic!("Unsupported color type {:?}", texture.color());
+            }
+        };
+
+        println!("Loaded texture of format {:#?}", format);
+        println!(
+            "GL_RED = {:?}, GL_RGB = {:?}, GL_RGBA = {:?}",
+            gl::RED,
+            gl::RGB,
+            gl::RGBA
+        );
+
+        gl::GenTextures(1, &mut texture_id);
+        gl::BindTexture(gl::TEXTURE_2D, texture_id);
+        gl::TexImage2D(
+            gl::TEXTURE_2D,
+            0,
+            format as i32,
+            texture_w,
+            texture_h,
+            0,
+            format as u32,
+            gl::UNSIGNED_BYTE,
+            texture.as_bytes().as_ptr().cast(),
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::REPEAT as i32);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::REPEAT as i32);
+        gl::TexParameteri(
+            gl::TEXTURE_2D,
+            gl::TEXTURE_MIN_FILTER,
+            gl::LINEAR_MIPMAP_LINEAR as i32,
+        );
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    }
+
+    texture_id
 }
